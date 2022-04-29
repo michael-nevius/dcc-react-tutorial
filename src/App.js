afd0c76287be1d5caf45195b8181de6d7e5df9e1
michael-nevius <michael.a.nevius@gmail.com>
2022-04-29 16:43:01 -0400

    Completed the Weighttracker app it is fully fucntioning

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
 import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
-
+import './App.css'
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}])
@@ -13,10 +13,24 @@ function App() {
   }
 
   return (
-    <div>
-      <DisplayEntries parentEntries={entries} />
-      <AddEntryForm addNewEntryProperty={addNewEntry} />
-      <EntriesChartTracker parentEntries={entries} />
+    <div className='container-fluid'>
+      <div className='row'>
+        <h3 style={{margin: '1em'}}>Weight
+        <small className='text-muted'>Tracker</small></h3>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <DisplayEntries parentEntries={entries} />
+          </div>
+          <div className='border-box'>
+            <AddEntryForm addNewEntryProperty={addNewEntry} />
+          </div>  
+        </div>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <EntriesChartTracker parentEntries={entries} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
