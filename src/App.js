5039f0034267a0a9ac89617cbc9a63b8699e3ba8
michael-nevius <michael.a.nevius@gmail.com>
2022-04-29 16:11:20 -0400

    Completed Entry Chart Tracker Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
+
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}])
@@ -13,7 +15,8 @@ function App() {
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
-      <AddEntryForm addNewEntryProperty={addNewEntry}/>
+      <AddEntryForm addNewEntryProperty={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
     </div>
   );
 }
